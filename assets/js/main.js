847f664adc57aeed404177f8b8b971fa2e3b6447
joaovill <joao.pgkv@gmail.com>
2023-08-31 16:50:21 -0300

    some sections mobile for maeztra test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 $( document ).ready(function() {
-    console.log('funfo');
     $('#owl-home').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
-        navText:['<','>'],
+        navText:['',''],
         items: 1,
     })
+    if($(window).width() < 767) {
+        $('#owl-why').owlCarousel({
+            autoplay: true,
+            autoplaySpeed: 1500,
+            center: false,
+            dots: false,
+            items: 1,
+            margin:0,
+            nav:false,
+        })
+    }
 })
