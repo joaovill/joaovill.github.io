74de28c0c6766dc5041eeba3adf24ee7f0b6e594
joaovill <joao.pgkv@gmail.com>
2024-04-12 11:57:33 -0300

    refactor desktop, some changes mob

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,28 @@ $( document ).ready(function() {
         navText:['',''],
         items: 1,
     })
-    if($(window).width() < 767) {
+    $('#owl-products, #owl-products2').owlCarousel({
+        center: true,
+        dots: false,
+        items: 1,
+        margin:15,
+        nav:false,
+        responsive: {
+            0:{
+                items: 1
+            },
+            768:{
+                items: 3
+            },
+            1200:{
+                items: 5,
+                center:false,
+                nav:true,
+                navText:['','']
+            },
+        }
+    })
+    if(window.innerWidth < 768){
         $('#owl-why').owlCarousel({
             autoplay: true,
             autoplaySpeed: 1500,
@@ -14,14 +35,7 @@ $( document ).ready(function() {
             dots: false,
             items: 1,
             margin:0,
-            nav:false,
-        })
-        $('#owl-products').owlCarousel({
-            center: true,
-            dots: false,
-            items: 1,
-            margin:15,
-            nav:false,
+            nav:false
         })
     }
 })
